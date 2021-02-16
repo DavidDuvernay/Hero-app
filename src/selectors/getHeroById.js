6e0029972d16778e320e24ee085ea6bace5b7f96
David Duvernay <david.anderson0612@gmail.com>
2021-02-15 21:22:59 -0500

    Added SearchScreen and modified some components to work along with their animations

--- src/selectors/getHeroById.js
+++ src/selectors/getHeroById.js
@@ -2,5 +2,5 @@ const { heroes } = require("../data/heroes");
 
 export const getHeroesById = (id) => {
 
-    return heroes.filter(hero => hero.id === id )
+    return heroes.find(hero => hero.id === id )
 }
